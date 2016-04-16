33322da11ba437c7f0108734eceb287f71434443
andreas <schn.andreas@gmail.com>
2016-04-16 21:33:15 +0200

    added dots to 'create room'

--- FrontEnd/www/js/directives.js
+++ FrontEnd/www/js/directives.js
@@ -15,18 +15,25 @@ angular.module('app.directives', ['d3'])
               .on("mousedown", mousedown)
               .on("mouseup", mouseup);
 
+            for(var i=0;i<31;i++) {
+              for(var j=0;j<21;j++) {
+                circle = vis.append("circle")
+                  .attr("cx", i*20)
+                  .attr("cy", j*20)
+                  .attr("r", 0.5)
+
+              }
+            }
+
             function mousedown(e) {
               e = e || window.event;
               e.stopPropagation();
               var m = d3.mouse(this);
               line = vis.append("line")
-                .attr("x1", Math.round(m[0]/50)*50)
-                .attr("y1", Math.round(m[1]/50)*50)
-                .attr("x2", Math.round(m[0]/50)*50)
-                .attr("y2", Math.round(m[1]/50)*50);
-
-              console.log(Math.round(m[0]/50)*50);
-              console.log(Math.round(m[1]/50)*50);
+                .attr("x1", Math.round(m[0]/20)*20)
+                .attr("y1", Math.round(m[1]/20)*20)
+                .attr("x2", Math.round(m[0]/20)*20)
+                .attr("y2", Math.round(m[1]/20)*20);
 
               vis.on("mousemove", mousemove);
             }
@@ -36,8 +43,8 @@ angular.module('app.directives', ['d3'])
               e.stopPropagation();
               var m = d3.mouse(this);
               line.attr("class", "line")
-                .attr("x2", Math.round(m[0]/50)*50)
-                .attr("y2", Math.round(m[1]/50)*50);
+                .attr("x2", Math.round(m[0]/20)*20)
+                .attr("y2", Math.round(m[1]/20)*20);
             }
 
             function mouseup(e) {
