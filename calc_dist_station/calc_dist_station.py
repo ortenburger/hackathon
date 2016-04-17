e41d0b5705cc8b59f8a87dd836710cad3f02683d
Ortenburger <ortenburger@gmail.com>
2016-04-17 06:09:36 +0200

    now with distances.json in utf-8

--- calc_dist_station/calc_dist_station.py
+++ calc_dist_station/calc_dist_station.py
@@ -5,7 +5,7 @@ calc_dist_station
 Usage:
 	calc_dist_station.py langitude longitude (in float)
 """
-
+import io
 import json
 import functools
 import sys
@@ -27,8 +27,8 @@ def calc_dist(args):
 			for station in stations:
 				station['distance'] = vincenty(args[0] + ", " + args[1], station["latitude"] + ", " + station["longitude"]).km
 			sorted_stations = sorted(stations, key=lambda k: k['distance']) 
-			with open('distances.json', 'w') as f:
-				json.dump(sorted_stations, f)
+			with io.open('distances.json', 'w', encoding='utf8') as outfile:
+				json.dump(sorted_stations, outfile, ensure_ascii=False)
 	else: exit()
 
 
