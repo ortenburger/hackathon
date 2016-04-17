a1ce87e24a575d24b935ddcb487cd4f723f5ca91
JonasJustus <travel.florian@gmail.com>
2016-04-17 08:08:48 +0200

    Wake up Timer

--- FrontEnd/www/js/controllers.js
+++ FrontEnd/www/js/controllers.js
@@ -4,7 +4,27 @@ angular.module('app.controllers', [])
 
 })
 
-.controller('wakeUpAlarmCtrl', function($scope) {
+.controller('wakeUpAlarmCtrl', function($scope, ionicTimePicker) {
+
+$scope.opentimer=function(){
+  console.log("scope click");
+  var ipObj1 = {
+  callback: function (val) {      //Mandatory
+  if (typeof (val) === 'undefined') {
+    console.log('Time not selected');
+  } else {
+     $scope.selectedTime = new Date(val * 1000);
+    console.log('Selected epoch is : ', val, 'and the time is ', selectedTime.getUTCHours(), 'H :', selectedTime.getUTCMinutes(), 'M');
+  }
+  },
+  inputTime: 50400,   //Optional
+  format: 24,         //Optional
+  step: 5,           //Optional
+  setLabel: 'Set'    //Optional
+  };
+ionicTimePicker.openTimePicker(ipObj1);
+
+};
 
 })
 
